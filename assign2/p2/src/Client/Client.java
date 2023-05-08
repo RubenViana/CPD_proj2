a6ba0bc2ef4cf6f1a72d35ca60c15839d653b6a7
ruben <up202005108@up.pt>
2023-05-08 10:32:10 +0100

    new way

--- assign2/p2/src/Client/Client.java
+++ assign2/p2/src/Client/Client.java
@@ -4,62 +4,54 @@ import java.net.*;
 import java.io.*;
 import java.util.Scanner;
 
-/**
- * This program demonstrates a simple TCP/IP socket client.
- *
- * @author www.codejava.net
- */
 public class Client {
- 
-    public static void main(String[] args) throws IOException{
-        if (args.length < 2) return;
- 
-        String hostname = args[0];
-        int port = Integer.parseInt(args[1]);
 
-        // establish a connection by providing host and port
-        // number
-        try (Socket socket = new Socket(hostname, port)) {
+    String serverAddress;
+    Integer serverPort;
+    Scanner in;
+    PrintWriter out;
+    String name;
 
-            // writing to server
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+    /**
+     * Constructs the client by laying out the GUI and registering a listener with
+     * the textfield so that pressing Return in the listener sends the textfield
+     * contents to the server. Note however that the textfield is initially NOT
+     * editable, and only becomes editable AFTER the client receives the
+     * NAMEACCEPTED message from the server.
+     */
+    public Client(String serverAddress, String serverPort) {
+        this.serverAddress = serverAddress;
+        this.serverPort = Integer.parseInt(serverPort);
+    }
 
-            // reading from server
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            // object of scanner class
-            Scanner sc = new Scanner(System.in);
+    private void run() throws IOException {
+        try {
+            var socket = new Socket(serverAddress, serverPort);
+            in = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream(), true);
 
-            String line = null;
-
-            while (true) {
-                // displaying server reply
-                if ((in.ready()))
-                    System.out.println("Server> " + in.readLine());
-
-                // reading from user
-
-                line = sc.nextLine();
-                // sending the user input to server
-                out.println(line);
-                out.flush();
-
-                if (line.equals("exit")) {
-                    System.out.println("Closing this connection : " + socket);
-                    socket.close();
-                    System.out.println("Connection closed");
-                    break;
+            while (in.hasNextLine()) {
+                var line = in.nextLine();
+                if (line.startsWith("SUBMITNAME")) {
+                    System.out.println("Server> Choose a server name!");
+                } else if (line.startsWith("NAMEACCEPTED")) {
+                    this.name = line.substring(13);
+                } else if (line.startsWith("MESSAGE")) {
+                    System.out.println(this.name + "> " + line.substring(8));
                 }
-
             }
+        } finally {
+            System.out.println("\n");
+        }
+    }
 
-            // closing the scanner, in, out objects
-            sc.close();
-            in.close();
-            out.close();
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Pass the server IP as the sole command line argument");
+            return;
         }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+        var client = new Client(args[0], args[1]);
+        client.run();
     }
 }
